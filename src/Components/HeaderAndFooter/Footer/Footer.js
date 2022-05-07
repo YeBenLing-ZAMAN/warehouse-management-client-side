d9a227733d16f779f5ba9ad0125a41c4bdc44877
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-05-07 08:58:31 +0600

    header and footer basic structure is done

---
+++ src/Components/HeaderAndFooter/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-dark text-light text-center'>
+            <p>This is footer @ zaman</p>
+        </div>
+    );
+};
+
+export default Footer;
